2691ca0ec392182811fe2bc8b6658c11eca4d3c5
TurtlePower <agnkvdm@gmail.com>
2023-03-19 18:54:43 +0200

    second hw

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,12 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+ingredients.forEach(ingredient => {
+  const ingredientItemEl = document.createElement('li');
+  ingredientItemEl.textContent = `${ingredient}`;
+  ingredientItemEl.classList.add('item');
+
+  console.log(ingredientItemEl);
+
+  const ingredientListEl = document.querySelector('ul#ingredients');
+  ingredientListEl.append(ingredientItemEl);
+});
