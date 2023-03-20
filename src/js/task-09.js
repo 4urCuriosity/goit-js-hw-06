b06b8b46579a801bed1e9d94ab4bd536382b9578
TurtlePower <agnkvdm@gmail.com>
2023-03-20 18:11:26 +0200

    9th hw

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,15 @@
+const bodyEl = document.querySelector('body');
+const colorNameEl = document.querySelector('span.color');
+const changeColorBtn = document.querySelector('button.change-color');
+
+changeColorBtn.addEventListener('click', onClickChangeColorBtn);
+
+function onClickChangeColorBtn() {
+  const hexColor = getRandomHexColor();
+  bodyEl.style.backgroundColor = hexColor;
+  colorNameEl.textContent = hexColor;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
