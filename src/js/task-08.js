17fc1960916ac1f0bfedf9521b2d2f216c0834a2
TurtlePower <agnkvdm@gmail.com>
2023-03-20 17:53:44 +0200

    8th hw

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,20 @@
+const formEl = document.querySelector('form.login-form');
 
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const { email, password } = event.currentTarget.elements;
+  if (email.value === '' || password.value === '') {
+    alert('Все поля должны быть заполнены!');
+    return;
+  }
+
+  console.log({
+    [email.name]: email.value,
+    [password.name]: password.value,
+  });
+
+  event.currentTarget.reset();
+}
